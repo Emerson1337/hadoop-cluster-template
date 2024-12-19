722928becf20211b7bbd4b5bc3d55d1744c19f66
Ives Moreira <ivesmoreira3@gmail.com>
2024-12-18 22:28:40 -0300

    fix: adjust tcpdump script

--- script-tcpdump/index.py
+++ script-tcpdump/index.py
@@ -1,5 +1,7 @@
 import subprocess
 import time
+import csv
+import os
 
 def executar_por_tempo(tempo_maximo):
     inicio = time.time()
@@ -14,9 +16,27 @@ executar_por_tempo(30)
 tcpdump.terminate()
 tcpdump.wait()
 
-tshark = subprocess.Popen('tshark -r output.pcap -T fields -e frame.number -e ip.src -e ip.dst -E separator=";" > saida.csv', shell=True)
+tshark = subprocess.Popen('tshark -r output.pcap -T fields -e frame.time -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e tcp.flags.str -e tcp.seq -e tcp.ack -e tcp.window_size_value -e tcp.options -e tcp.len -E separator=, > saida.csv', shell=True)
 
 executar_por_tempo(30)
 
 tshark.terminate()
 tshark.wait()
+
+arquivo_csv = 'saida.csv'
+arquivo_temp = 'arquivo_temp.csv'
+
+titulos = ['Coluna1', 'Coluna2', 'Coluna3', 'Coluna4', 'Coluna5', 'Coluna6', 'Coluna7', 'Coluna8', 'Coluna9', 'Coluna10', 'Coluna11',] 
+
+with open(arquivo_csv, 'r', newline='', encoding='utf-8') as arquivo_original, \
+     open(arquivo_temp, 'w', newline='', encoding='utf-8') as arquivo_modificado:
+
+    leitor = csv.reader(arquivo_original)
+    escritor = csv.writer(arquivo_modificado)
+
+    escritor.writerow(titulos)
+
+    for linha in leitor:
+        escritor.writerow(linha)
+
+os.replace(arquivo_temp, arquivo_csv)
